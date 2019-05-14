c6126eb0d0d1d06fb7a8b25d3fcb5d3d1c18d503
haifeiWu <whfstudio@163.com>
2019-05-14 20:23:38 +0800

    interface design

--- src/main/java/com/whfstudio/graphbed/web/GraphController.java
+++ src/main/java/com/whfstudio/graphbed/web/GraphController.java
@@ -7,10 +7,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wuhaifei 2019-05-13
  */
 @RestController
+@RequestMapping("/v1")
 public class GraphController {
 
     @RequestMapping(value = "/hello")
     public String hello() {
         return "hello";
     }
+
+    @RequestMapping(value = "/list")
+    public String list() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/rename")
+    public String rename() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/search")
+    public String search() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/upload")
+    public String upload() {
+        return "hello";
+    }
+
 }
