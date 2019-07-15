480a7824980bbdebd7111b79bdfd308997c62cc8
haifeiWu <whfstudio@163.com>
2019-07-15 10:38:59 +0800

    update

--- graph-bed-web/src/main/java/com/whfstudio/graphbed/web/GraphController.java
+++ graph-bed-web/src/main/java/com/whfstudio/graphbed/web/GraphController.java
@@ -1,14 +1,20 @@
 package com.whfstudio.graphbed.web;
 
 
+import com.whfstudio.graphbed.common.model.GraphInfo;
 import com.whfstudio.graphbed.service.DemoService;
+import com.whfstudio.graphbed.service.GraphService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * @author wuhaifei 2019-05-13
  */
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 public class GraphController {
@@ -16,14 +22,20 @@ public class GraphController {
     @Reference(version = "1.0.0", url = "dubbo://127.0.0.1:12345")
     private DemoService demoService;
 
+    @Reference(version = "1.0.0", url = "dubbo://127.0.0.1:12345")
+    private GraphService graphService;
+
     @RequestMapping(value = "/hello")
     public String hello() {
         String hello = demoService.sayHello("");
+        log.info("result : [{}]",hello);
         return hello;
     }
 
     @RequestMapping(value = "/list")
     public String list() {
+        List<GraphInfo> graphInfoList = graphService.graphList();
+        log.info("result : [{}]");
         return "hello";
     }
 
